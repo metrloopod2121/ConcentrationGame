26acba9c44f44610b189a4c767a585180bc3336e
Matvey <metrloopod2121@mail.ru>
2023-10-26 23:36:47 +0300

    Initial Commit

---
+++ ConcentrationGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ConcentrationGame
+//
+//  Created by 𝕄𝕒𝕥𝕧𝕖𝕪 ℙ𝕠𝕕𝕘𝕠𝕣𝕟𝕚𝕪 on 26.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
